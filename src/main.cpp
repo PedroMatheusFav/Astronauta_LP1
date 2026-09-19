8f5fbccb22befed233063e5b261905469ede38dd
PedroMatheus <pedromatheusramosoliveira@gmail.com>
2026-09-18 23:44:15 -0300

    atualização 2

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,92 @@ using namespace std;
 
 // Parte 1: escreva aqui as classes Astronauta, Voo e Agencia.
 class Astronauta {
-    string cpf;
-    string nome;
-    int idade;
+    private:
+        string cpf;
+        string nome;
+        int idade;
+
+    public:
+        Astronauta(string c, string n, int i){
+            cpf = c;
+            nome = n;
+            idade = i;
+        }
 };
 
 class Voo {
-    int codigo;
-    vector<Astronauta> astronautas;
-    bool lancado;
-    bool finalizado;
-    bool explodido;
+    private:
+        int codigo;
+
+    public:
+        Voo(int c){
+            codigo = c;
+        }
+};
+
+class Morte {
+    private:
+        string cpf;
+        int nome;
+    public:
+        Morte(string c, int v){
+            cpf = c;
+            nome = v;
+        }
 };
 
 class Agencia {
-    vector<Astronauta> astronautas;
-    vector<Voo> voos;
+    private:
+        vector<Astronauta> astronautas;
+        vector<Voo> voos;
+        vector<Morte> mortos;
+    
+    public:
+        Agencia(){
+        }
+
+        void cadastrarAstronauta(string c, string n, int i){
+            astronautas.push_back(Astronauta(c, n, i));
+            cout << "OK: astronauta " << c << " cadastrado" << endl;
+        }
+
+        void adicionarAstronauta(string cpf, int codigo){
+            for(int i = 0; i < astronautas.size(); i++){
+                if(astronautas[i] == cpf){
+                    cout << "OK: astronauta " << cpf << " adicionado ao voo " << codigo << endl;
+                    return;
+                    break;
+                } else if (i == astronautas.size() - 1){
+                cout << "ERRO: astronauta " << cpf << " nao encontrado" << endl;
+                return;
+                }
+            }
+        }
+
+        void cadastrarVoo(int codigo){
+            voos.push_back(Voo(codigo));
+            cout << "OK: voo " << codigo << " cadastrado" << endl;
+        }
+
+        void explodirVoo(int codigo){
+            voos.push_back(Voo(codigo));
+            cout << "OK: voo " << codigo << " cadastrado" << endl;
+        }
+
+        void lancarVoo(int codigo){
+            for(int i = 0; i < voos.size(); i++){
+                if(voos[i] == codigo){
+                    cout << "OK: voo " << codigo << " lancado" << endl;
+                    return;
+                    break;
+                } else if (i == voos.size() - 1){
+                cout << "ERRO: voo " << codigo << " nao encontrado" << endl;
+                return;
+                }
+            }
+        }
+
+
 };
     
 
@@ -41,12 +111,12 @@ int main() {
             int idade;
             cin >> cpf >> idade;
             getline(cin >> ws, nome);   // o nome vem por ultimo e pode ter espacos
-            cout << "TODO " << comando << endl;
+            cout << agencia.cadastrarAstronauta(cpf, nome, idade) << comando << endl;
             // TODO: agencia.cadastrarAstronauta(cpf, nome, idade);
         } else if (comando == "CADASTRAR_VOO") {
             int codigo;
             cin >> codigo;
-            cout << "TODO " << comando << endl;
+            cout << agencia.cadastrarVoo(codigo) << comando << endl;
             // TODO: agencia.cadastrarVoo(codigo);
         } else if (comando == "ADICIONAR_ASTRONAUTA") {
             string cpf;
